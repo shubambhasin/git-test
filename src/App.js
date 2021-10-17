2daa1ce49e7ed3b58cf3ccaf9bee30a59eeeeaac
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-10-17 23:53:57 +0530

    feat one complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const feat1 = () => {
+    console.log("Feature one executed");
+  };
   return (
     <div className="App">
-      Git test app
+      Git test app, check console
+      <button onClick={() => feat1()}>Feature 1</button>
     </div>
   );
 }
