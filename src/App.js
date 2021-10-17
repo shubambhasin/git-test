746c1df8882d83bd060003d58b93a6d817b489cc
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-10-17 23:59:06 +0530

    feature 2 complete

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   const feat1 = () => {
     console.log("Feature one executed");
   };
+  const feat2 = () => {
+    console.log("Feature two executed");
+  };
   return (
     <div className="App">
       Git test app, check console
       <button onClick={() => feat1()}>Feature 1</button>
+      <button onClick={() => feat2()}>Feature 2</button>
     </div>
   );
 }
